3cc8315c6d3385e10c37db70d7c3bd376050b9b3
Expiredmil <101585765+Expiredmil@users.noreply.github.com>
2023-04-21 15:19:12 +0200

    timeout handling and rcvd corrections

--- btcp/constants.py
+++ btcp/constants.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb099b0af6eb3c069ae42a0473208ac9edcbb3ec48f6e014a165ce4c7868438f
-size 1024
+oid sha256:37e59f46db2d59438b59a5e62bcd84edad2c2f1936190a044dd472db2c8d70b1
+size 1037
